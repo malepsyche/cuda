f8732f63dc520002fbf569b4c55111606eb50dea
malepsyche <114489469+malepsyche@users.noreply.github.com>
2026-03-22 23:38:35 +0800

    Ammend wave2d

--- run.sh
+++ run.sh
@@ -4,17 +4,22 @@ set -e
 cmake -S . -B build
 cmake --build build -j
 
-echo "Running vector_addition"
-./build/vector_addition
+# echo "Running vector_addition"
+# ./build/vector_addition
 
-echo "Running matrix_addition"
-./build/matrix_addition
+# echo "Running matrix_addition"
+# ./build/matrix_addition
 
-echo "Running matrix_multiplication_untiled"
-./build/matrix_multiplication_untiled
+# echo "Running matrix_multiplication_untiled"
+# ./build/matrix_multiplication_untiled
 
-echo "Running wave2d_naive"
-./build/wave2d_naive
+nvcc main.cu -o wave2d -lcusparse
 
-echo "Running wave2d_tiled"
-./build/wave2d_tiled
+rm -f results.csv
+
+for L in 1 2 4 8
+do
+    ./wave2d $L
+done
+
+python visualize.py
