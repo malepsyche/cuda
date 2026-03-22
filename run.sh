f473356bbe8cd16e9172e174ec65f9920b85fd80
malepsyche <114489469+malepsyche@users.noreply.github.com>
2026-03-23 00:02:08 +0800

    Remove garbage

--- run.sh
+++ run.sh
@@ -4,22 +4,12 @@ set -e
 cmake -S . -B build
 cmake --build build -j
 
-# echo "Running vector_addition"
-# ./build/vector_addition
+echo "Running vector_addition"
+./build/vector_addition
 
-# echo "Running matrix_addition"
-# ./build/matrix_addition
+echo "Running matrix_addition"
+./build/matrix_addition
 
-# echo "Running matrix_multiplication_untiled"
-# ./build/matrix_multiplication_untiled
+echo "Running matrix_multiplication_untiled"
+./build/matrix_multiplication_untiled
 
-nvcc main.cu -o wave2d -lcusparse
-
-rm -f results.csv
-
-for L in 1 2 4 8
-do
-    ./wave2d $L
-done
-
-python visualize.py
